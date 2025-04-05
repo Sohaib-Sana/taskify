b6cdd47542fb2e60fac5e6122d568c5a5f6b40de
Sohaib Sana <sohaib@highersummit.com>
2025-04-06 00:22:58 +0500

    Project Setup. - Readme with plan has added. - Setup Github Workflow Actions

---
+++ android/app/src/main/kotlin/com/sohaib/taskify/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.sohaib.taskify
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Set preferred orientations
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Taskify',
+      debugShowCheckedModeBanner: false,
+      home: HomeWidget(),
+    );
+  }
+}
+
+class HomeWidget extends StatelessWidget {
+  const HomeWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text("HomePage Of Taskify")));
+  }
+}
