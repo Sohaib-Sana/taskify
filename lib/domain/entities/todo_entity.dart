90b8d83738301922bbcef26d663be5a84fb249ff
Sohaib Sana <dev.sohaibsana@gmail.com>
2025-04-06 11:47:34 +0500

    Define domain setup

---
+++ lib/domain/entities/todo_entity.dart
@@ -0,0 +1,58 @@
+import 'package:equatable/equatable.dart';
+
+part './sub_entities.dart';
+
+class TodoEntity extends Equatable {
+  final int id;
+  final String title;
+  final String description;
+  final bool completionStatus;
+  final DateTime createdAt;
+  final DateTime completedAt;
+  final Priority priority;
+  const TodoEntity({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.completionStatus,
+    required this.createdAt,
+    required this.completedAt,
+    required this.priority,
+  });
+
+  TodoEntity copyWith({
+    int? id,
+    String? title,
+    String? description,
+    bool? completionStatus,
+    DateTime? createdAt,
+    DateTime? completedAt,
+    Priority? priority,
+  }) {
+    return TodoEntity(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      description: description ?? this.description,
+      completionStatus: completionStatus ?? this.completionStatus,
+      createdAt: createdAt ?? this.createdAt,
+      completedAt: completedAt ?? this.completedAt,
+      priority: priority ?? this.priority,
+    );
+  }
+
+  @override
+  List<Object> get props {
+    return [
+      id,
+      title,
+      description,
+      completionStatus,
+      createdAt,
+      completedAt,
+      priority,
+    ];
+  }
+
+  @override
+  bool get stringify => true;
+}
---
+++ lib/domain/repositories/todo_repository.dart
@@ -0,0 +1,8 @@
+abstract class TodoRepository {
+  /// Get all todos
+  /// Get a specific todo by ID
+  /// Add a new todo
+  /// Update an existing todo
+  /// Delete a todo by id
+  /// Toggle todo status as completed or incompleted.
+}
