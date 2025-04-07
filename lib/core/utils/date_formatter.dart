b724f63ca7010bc44a6783c58cc7c744ae19d75d
Sohaib Sana <dev.sohaibsana@gmail.com>
2025-04-07 23:31:09 +0500

    - Date Formatter is added.

---
+++ lib/core/utils/date_formatter.dart
@@ -0,0 +1,36 @@
+import 'package:intl/intl.dart';
+
+class DateFormatter {
+  static String formatDate(DateTime date) {
+    return DateFormat('MMM dd, yyyy').format(date);
+  }
+  
+  static String formatTime(DateTime date) {
+    return DateFormat('hh:mm a').format(date);
+  }
+  
+  static String formatDateTime(DateTime date) {
+    return DateFormat('MMM dd, yyyy - hh:mm a').format(date);
+  }
+  
+  static String formatRelative(DateTime date) {
+    final now = DateTime.now();
+    final difference = now.difference(date);
+    
+    if (difference.inDays > 365) {
+      return '${(difference.inDays / 365).floor()} years ago';
+    } else if (difference.inDays > 30) {
+      return '${(difference.inDays / 30).floor()} months ago';
+    } else if (difference.inDays > 7) {
+      return '${(difference.inDays / 7).floor()} weeks ago';
+    } else if (difference.inDays > 0) {
+      return '${difference.inDays} days ago';
+    } else if (difference.inHours > 0) {
+      return '${difference.inHours} hours ago';
+    } else if (difference.inMinutes > 0) {
+      return '${difference.inMinutes} minutes ago';
+    } else {
+      return 'Just now';
+    }
+  }
+}
