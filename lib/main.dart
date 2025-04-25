891b30b5b67aa40e3e8f05edf9f027e15f2dbc4c
Sohaib Sana <dev.sohaibsana@gmail.com>
2025-04-25 16:27:00 +0500

    Theme modes are working now.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:taskify/core/config/routes/router.dart';
 import 'package:taskify/core/config/themes/app_theme.dart';
+import 'package:taskify/core/config/themes/cubit/theme_cubit.dart';
 import 'package:taskify/data/datasources/local/todo_local_db.dart';
 import 'package:taskify/data/datasources/todo_datasource.dart';
 import 'package:taskify/data/repositories/todo_repository_impl.dart';
@@ -14,29 +16,36 @@ import 'core/usecases.dart/usecase_barrel.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Set preferred orientations
+  // Lock device orientation
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
 
-  runApp(const MyApp());
+  // Initialize SharedPreferences before runApp
+  final sharedPreferences = await SharedPreferences.getInstance();
+
+  runApp(MyApp(sharedPreferences: sharedPreferences));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final SharedPreferences sharedPreferences;
+
+  const MyApp({super.key, required this.sharedPreferences});
 
   @override
   Widget build(BuildContext context) {
     return MultiRepositoryProvider(
       providers: [
-        RepositoryProvider<TodoDatasource>(create: (context) => TodoLocalDb()),
+        RepositoryProvider<TodoDatasource>(create: (_) => TodoLocalDb()),
         RepositoryProvider<TodoRepository>(
           create:
               (context) => TodoRepositoryImpl(
                 datasource: context.read<TodoDatasource>(),
               ),
         ),
+        // Provide SharedPreferences instance
+        RepositoryProvider<SharedPreferences>.value(value: sharedPreferences),
       ],
       child: MultiBlocProvider(
         providers: [
@@ -52,13 +61,25 @@ class MyApp extends StatelessWidget {
                   deleteTodoUseCase: DeleteTodo(context.read<TodoRepository>()),
                 ),
           ),
+          BlocProvider<ThemeCubit>(
+            create:
+                (context) => ThemeCubit(
+                  sharedPreferences: context.read<SharedPreferences>(),
+                ),
+          ),
         ],
-        child: MaterialApp(
-          title: 'Taskify',
-          theme: AppTheme.lightTheme,
-          debugShowCheckedModeBanner: false,
-          initialRoute: '/splash',
-          onGenerateRoute: AppRouter.onGenerateRoute,
+        child: BlocBuilder<ThemeCubit, ThemeState>(
+          builder: (context, state) {
+            return MaterialApp(
+              title: 'Taskify',
+              theme: AppTheme.lightTheme,
+              darkTheme: AppTheme.darkTheme,
+              themeMode: state.themeMode, // 👈 dynamic theming applied here
+              debugShowCheckedModeBanner: false,
+              initialRoute: '/splash',
+              onGenerateRoute: AppRouter.onGenerateRoute,
+            );
+          },
         ),
       ),
     );
