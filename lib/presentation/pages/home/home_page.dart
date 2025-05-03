e05924cc985cb0203d842956ac15fb2f5246a8ca
Sohaib Sana <dev.sohaibsana@gmail.com>
2025-05-03 16:57:07 +0500

    Removed back button from app bar on Home Screen

--- lib/presentation/pages/home/home_page.dart
+++ lib/presentation/pages/home/home_page.dart
@@ -69,6 +69,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         title: Text(AppConstants.appName),
         actions: [
           IconButton(
